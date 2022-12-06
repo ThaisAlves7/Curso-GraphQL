a3126e679f1f308c9fcb92d9b66e62fe839ba138
Thais Alves <thacruzz007@gmail.com>
2022-12-06 20:44:09 -0300

    Object Types [ Custom Types ]

--- Secao003/src/index.js
+++ Secao003/src/index.js
@@ -1,24 +1,69 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+// const server = new ApolloServer({
+//   typeDefs: gql`
+//     type Query {
+//       id: ID!
+//       name: String!
+//       age: Int!
+//       average: Float!
+//       married: Boolean! #Ao inserir ! você torna o objeto OBRIGATÓRIO
+//       arrayString: [String!]! #Impedi que dentro Array retorne NULL dentro
+//     }
+//   `,
+//   resolvers: {
+//     Query: {
+//       id: () => 'iasd987-7-b',
+//       name: () => 'Thais Alves',
+//       age: () => 29,
+//       average: () => 50.55,
+//       married: () => false,
+//       arrayString: () => ['A', 'B'],
+//     },
+//   },
+// });
+
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      id: ID!
-      name: String!
-      age: Int!
-      average: Float!
-      married: Boolean! #Ao inserir ! você torna o objeto OBRIGATÓRIO
-      arrayString: [String!]! #Impedi que dentro Array retorne NULL dentro
+      user: User!
+      users: [User!]!
+    }
+
+    type User {
+      id: ID
+      userName: String
     }
   `,
   resolvers: {
     Query: {
-      id: () => 'iasd987-7-b',
-      name: () => 'Thais Alves',
-      age: () => 29,
-      average: () => 50.55,
-      married: () => false,
-      arrayString: () => ['A', 'B'],
+      user: () => {
+        return {
+          id: 'asd8708',
+          userName: 'thais_alves',
+        };
+      },
+
+      users: () => {
+        return [
+          {
+            id: '1',
+            userName: 'thais_alves',
+          },
+          {
+            id: '2',
+            userName: 'bia_burguesa',
+          },
+          {
+            id: '3',
+            userName: 'wanda_maximoff',
+          },
+          {
+            id: '4',
+            userName: 'natasha_romanoff',
+          },
+        ];
+      },
     },
   },
 });
