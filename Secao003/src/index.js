962d2115427fd97c85d91fb0b566f3d0133941de
Thais Alves <thacruzz007@gmail.com>
2022-12-06 20:14:11 -0300

    Scalar Types [Padrão]

--- Secao003/src/index.js
+++ Secao003/src/index.js
@@ -1,1 +1,28 @@
-console.log();
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      id: ID!
+      name: String!
+      age: Int!
+      average: Float!
+      married: Boolean! #Ao inserir ! você torna o objeto OBRIGATÓRIO
+      arrayString: [String!]! #Impedi que dentro Array retorne NULL dentro
+    }
+  `,
+  resolvers: {
+    Query: {
+      id: () => 'iasd987-7-b',
+      name: () => 'Thais Alves',
+      age: () => 29,
+      average: () => 50.55,
+      married: () => false,
+      arrayString: () => ['A', 'B'],
+    },
+  },
+});
+
+server
+  .listen(4003)
+  .then(({ url }) => console.log(`Server listening on url ${url}`));
